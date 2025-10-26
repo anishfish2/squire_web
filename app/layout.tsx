4fe156dc570b5c9a5cf6efe3746bf5d7e66ec754
Anish Karthik <anishkarthik21@gmail.com>
2025-10-26 18:59:49 -0500

    added favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.png" type="image/png" />
+
+      </head>
       <body className="antialiased">
         {children}
       </body>
